7d5d87da662eb4385f1b3cbee2e26e54c6456a19
mohdarif7700 <47075030+mohdarif7700@users.noreply.github.com>
2019-03-17 15:15:34 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,6 +110,10 @@ var count1bb = 0;
 var count2bb = 0;
 /** @type {number} */50
 var count3bb = 0;
+.then(function (message) {
+          message.react("👍")
+          message.react("👎")
+              });
 var gamestartedswoo;
 var gamestartedlo;
 var gamestartedbb;
